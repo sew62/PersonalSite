8334a4310fcd8d8599c652c76f5bd569f5a6d11e
Scott Williams <sew62@zips.uakron.edu>
2018-08-13 21:22:24 -0400

    Cleanup and changes to background images/link hovers

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom';
+
+var selectedButtonStyle = {
+  textDecoration: "underline"
+};
 
 class NavBar extends React.Component {
   state = {
@@ -7,7 +11,6 @@ class NavBar extends React.Component {
   }
 
   toggleMenu() {
-
     this.setState({
       disp: this.state.disp === "none" ? "inherit" : "none"
     });
@@ -16,25 +19,31 @@ class NavBar extends React.Component {
   render() {
     return (
       <div id="my-nav">
-        <nav>
-          <Link to="/"><img id="avatar" alt="Scott's Avatar" src="images/myicon.png"/>&nbsp;Scott Williams</Link>
+        <nav> 
+          <Link to="/" className="nav-button"><img id="avatar" alt="Scott's Avatar" src="images/myicon.png"/>&nbsp;Scott Williams</Link>
           <span className="right-align">
             <span id="nav-links">
-              <Link to="/">Home</Link>&nbsp;|&nbsp;
-              <Link to="/contact">Contact</Link>&nbsp;|&nbsp; 
-              <Link to="/portfolio">Portfolio</Link>
+              <Link className="nav-button" to="/">
+                Home
+              </Link>&nbsp;|&nbsp;
+              <Link className="nav-button" to="/contact">
+                Contact
+              </Link>&nbsp;|&nbsp; 
+              <Link className="nav-button" to="/portfolio">
+                Portfolio
+              </Link>
             </span>
             <button id="menu-icon" onClick={this.toggleMenu.bind(this)}><img src="images/menuicon.svg" alt="Menu Icon"/></button>
           </span>
         </nav>
-          <div id="menu-links" style={{display: this.state.disp}}>
-            <div className="menu-li" style={{marginTop: "15px"}}><Link to="/">Home</Link></div>
-            <div className="menu-li"><Link to="/contact">Contact</Link></div>
-            <div className="menu-li"><Link to="/portfolio">Portfolio</Link></div>
-          </div>
+        <div id="menu-links" style={{display: this.state.disp}}>
+          <div className="menu-li" style={{marginTop: "15px"}}><Link to="/">Home</Link></div>
+          <div className="menu-li"><Link to="/contact">Contact</Link></div>
+          <div className="menu-li"><Link to="/portfolio">Portfolio</Link></div>
+        </div>
       </div>
     );
   }
 };
 
-export default NavBar;
+export default withRouter(NavBar);
